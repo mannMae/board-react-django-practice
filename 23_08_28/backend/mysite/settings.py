dca8fb42091a43a35b106e55b25c806b11d7edc8
Jaeman Kim <daga4242@gmail.com>
2023-08-28 15:06:02 +0900

    backend

--- 23_08_28/backend/mysite/settings.py
+++ 23_08_28/backend/mysite/settings.py
@@ -30,7 +30,15 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+THIRD_PARTY_APPS = [
+    "rest_framework"
+]
+
+CUSTOM_APPS = [
+    "posts.apps.PostsConfig"
+]
+
+SYSTEM_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,6 +46,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+INSTALLED_APPS = THIRD_PARTY_APPS + CUSTOM_APPS + SYSTEM_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
