4b11df726e54d6d2140d7905d94806ea50d053e7
Jaeman Kim <daga4242@gmail.com>
2023-09-01 21:02:55 +0900

    23_09_01 backend

---
+++ 23_09_01/backend/posts/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+
+from .models import Post
+from .serializers import PostsSerializer, PostDetailSerializer
+
+# Create your views here.
+
+class Posts(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostsSerializer(posts, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = PostDetailSerializer(data=request.data)
+        if serializer.is_valid():
+            title = request.data.get("title")
+            content = request.data.get("content")
+            serializer.save(title=title, content=content)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+        
+class PostDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            return NotFound  
+    
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        return Response(PostDetailSerializer(post).data)
+    
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostDetailSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response("deleted")
